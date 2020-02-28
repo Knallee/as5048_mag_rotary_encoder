060d768929c15973e25dfc11d00cd4b2197f63f7
Christoffer Cederberg <christoffer.cederberg@eit.lth.se>
2020-02-28 21:32:10 +0100

    first commit, setting up repo

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * as5048_mag_rotary_encoder.c
+ *
+ * Created: 2020-02-28 21:30:07
+ * Author : Christoffer
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
